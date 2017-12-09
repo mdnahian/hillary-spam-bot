58da93e935f96686e59ffa6acaf3e89340720528
Ubuntu <ubuntu@ip-172-31-13-61.ec2.internal>
2017-12-09 19:42:02 +0000

    generate

--- hillary_model.py
+++ hillary_model.py
@@ -1,5 +1,6 @@
 import numpy as np, tflearn, sys, tensorflow as tf
-from tensorflow.contrib.keras.python.keras.utils import np_utils
+#from tensorflow.contrib.keras.python.keras.utils import np_utils
+from keras.utils import np_utils
 import sys
 
 filename = 'hillary'
@@ -30,19 +31,48 @@ def build_model():
 	net = tflearn.fully_connected(net, y.shape[1], activation='softmax')
 	net = tflearn.regression(net, optimizer='adam', learning_rate=0.005, loss='categorical_crossentropy')
 	
-	return tflearn.DNN(net, checkpoint_path='charmodel/model.tfl.ckpt'), train
+	return tflearn.DNN(net, checkpoint_path='charmodel/model.tfl.ckpt'), train, X, y, intchar
 
 
 
 def train():
-	model, train = build_model()
+	print('building...')
+	model, train, X, y = build_model()
+	print('training...')
 	model.fit(X, y, snapshot_epoch=True, snapshot_step=5000, n_epoch=20, batch_size = 128)
+	print('saving...')
 	model.save(filename)
 
 
 
 def run():
-	model, train = build_model()
+	input = open("hillary.txt").read().lower()
+	chars = sorted(list(set(input)))
+
+	charint = dict((char,ints) for ints, char in enumerate(chars))
+	intchar = dict((ints,char) for ints, char in enumerate(chars))
+
+	seqlen = 100
+	lstmhid=320
+	keeprate = 0.80
+	train = []
+	true = []
+	tf.reset_default_graph()
+
+	for i in range(0, len(input)-seqlen, 1):
+	    train.append([charint[char] for char in input[i:i+seqlen]])
+	    true.append(charint[input[i+seqlen]])
+
+	X = np.reshape(train, (len(train), seqlen, 1))/float(len(chars))
+	y = np_utils.to_categorical(true)
+
+	net = tflearn.input_data(shape=(None, X.shape[1], X.shape[2]))
+	net = tflearn.lstm(net, lstmhid)
+	net = tflearn.fully_connected(net, y.shape[1], activation='softmax')
+	net = tflearn.regression(net, optimizer='adam', learning_rate=0.005, loss='categorical_crossentropy')
+	
+	model = tflearn.DNN(net, checkpoint_path='charmodel/model.tfl.ckpt')
+
 	model.load(filename)
 	for _ in range(5):
 		p = train[np.random.randint(0,len(train)-1)]
